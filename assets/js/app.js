1c4599e4a7ce0214d7e8ac9960e18ab283c4c3e0
AryanSejani <73848092+AryanSejani@users.noreply.github.com>
2024-02-27 15:01:46 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -311,141 +311,141 @@ function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input ==
     });
   }); /*End document ready*/
 
-  $(window).on("resize", function () {}); // end window resize
+  $(window).on("resize", function () { }); // end window resize
 
-  $(window).on("load", function () {}); // End window LODE
+  $(window).on("load", function () { }); // End window LODE
 
   /*--------------------------------------------------------------
   AXIMO MAP JS
   ------------------------------------------------------------*/
-  var google_map = $('#map');
-  if (google_map.is_exist()) {
-    var init = function init() {
-      var mapOptions = {
-        zoom: 11,
-        scrollwheel: false,
-        navigationControl: false,
-        mapTypeControl: false,
-        scaleControl: false,
-        draggable: true,
-        disableDefaultUI: true,
-        center: new google.maps.LatLng(40.6700, -73.9400),
-        styles: [{
-          "featureType": "landscape.man_made",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#f7f1df"
-          }]
-        }, {
-          "featureType": "landscape.natural",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#d0e3b4"
-          }]
-        }, {
-          "featureType": "landscape.natural.terrain",
-          "elementType": "geometry",
-          "stylers": [{
-            "visibility": "off"
-          }]
-        }, {
-          "featureType": "poi",
-          "elementType": "labels",
-          "stylers": [{
-            "visibility": "off"
-          }]
-        }, {
-          "featureType": "poi.business",
-          "elementType": "all",
-          "stylers": [{
-            "visibility": "off"
-          }]
-        }, {
-          "featureType": "poi.medical",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#fbd3da"
-          }]
-        }, {
-          "featureType": "poi.park",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#bde6ab"
-          }]
-        }, {
-          "featureType": "road",
-          "elementType": "geometry.stroke",
-          "stylers": [{
-            "visibility": "off"
-          }]
-        }, {
-          "featureType": "road",
-          "elementType": "labels",
-          "stylers": [{
-            "visibility": "off"
-          }]
-        }, {
-          "featureType": "road.highway",
-          "elementType": "geometry.fill",
-          "stylers": [{
-            "color": "#ffe15f"
-          }]
-        }, {
-          "featureType": "road.highway",
-          "elementType": "geometry.stroke",
-          "stylers": [{
-            "color": "#efd151"
-          }]
-        }, {
-          "featureType": "road.arterial",
-          "elementType": "geometry.fill",
-          "stylers": [{
-            "color": "#ffffff"
-          }]
-        }, {
-          "featureType": "road.local",
-          "elementType": "geometry.fill",
-          "stylers": [{
-            "color": "black"
-          }]
-        }, {
-          "featureType": "transit.station.airport",
-          "elementType": "geometry.fill",
-          "stylers": [{
-            "color": "#cfb2db"
-          }]
-        }, {
-          "featureType": "water",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#a2daf2"
-          }]
-        }]
-      };
-      var mapElement = document.getElementById('map');
-      var map = new google.maps.Map(mapElement, mapOptions);
-      var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(40.6700, -73.9400),
-        map: map,
-        // icon: 'assets/images/all-img/contact/map.png',
-        title: 'fugu'
-      });
-      var contentString = '<div id="content">' + '<div id="tpw">' + '<h3>fugu' + '</div>';
-      var infowindow = new google.maps.InfoWindow({
-        content: contentString,
-        maxWidth: 280
-      });
-      marker.setAnimation(google.maps.Animation.BOUNCE);
-      setTimeout(function () {
-        marker.setAnimation(null);
-      }, 750); //time it takes for one bounce   
+  // var google_map = $('#map');
+  // if (google_map.is_exist()) {
+  //   var init = function init() {
+  //     var mapOptions = {
+  //       zoom: 11,
+  //       scrollwheel: false,
+  //       navigationControl: false,
+  //       mapTypeControl: false,
+  //       scaleControl: false,
+  //       draggable: true,
+  //       disableDefaultUI: true,
+  //       center: new google.maps.LatLng(40.6700, -73.9400),
+  //       styles: [{
+  //         "featureType": "landscape.man_made",
+  //         "elementType": "geometry",
+  //         "stylers": [{
+  //           "color": "#f7f1df"
+  //         }]
+  //       }, {
+  //         "featureType": "landscape.natural",
+  //         "elementType": "geometry",
+  //         "stylers": [{
+  //           "color": "#d0e3b4"
+  //         }]
+  //       }, {
+  //         "featureType": "landscape.natural.terrain",
+  //         "elementType": "geometry",
+  //         "stylers": [{
+  //           "visibility": "off"
+  //         }]
+  //       }, {
+  //         "featureType": "poi",
+  //         "elementType": "labels",
+  //         "stylers": [{
+  //           "visibility": "off"
+  //         }]
+  //       }, {
+  //         "featureType": "poi.business",
+  //         "elementType": "all",
+  //         "stylers": [{
+  //           "visibility": "off"
+  //         }]
+  //       }, {
+  //         "featureType": "poi.medical",
+  //         "elementType": "geometry",
+  //         "stylers": [{
+  //           "color": "#fbd3da"
+  //         }]
+  //       }, {
+  //         "featureType": "poi.park",
+  //         "elementType": "geometry",
+  //         "stylers": [{
+  //           "color": "#bde6ab"
+  //         }]
+  //       }, {
+  //         "featureType": "road",
+  //         "elementType": "geometry.stroke",
+  //         "stylers": [{
+  //           "visibility": "off"
+  //         }]
+  //       }, {
+  //         "featureType": "road",
+  //         "elementType": "labels",
+  //         "stylers": [{
+  //           "visibility": "off"
+  //         }]
+  //       }, {
+  //         "featureType": "road.highway",
+  //         "elementType": "geometry.fill",
+  //         "stylers": [{
+  //           "color": "#ffe15f"
+  //         }]
+  //       }, {
+  //         "featureType": "road.highway",
+  //         "elementType": "geometry.stroke",
+  //         "stylers": [{
+  //           "color": "#efd151"
+  //         }]
+  //       }, {
+  //         "featureType": "road.arterial",
+  //         "elementType": "geometry.fill",
+  //         "stylers": [{
+  //           "color": "#ffffff"
+  //         }]
+  //       }, {
+  //         "featureType": "road.local",
+  //         "elementType": "geometry.fill",
+  //         "stylers": [{
+  //           "color": "black"
+  //         }]
+  //       }, {
+  //         "featureType": "transit.station.airport",
+  //         "elementType": "geometry.fill",
+  //         "stylers": [{
+  //           "color": "#cfb2db"
+  //         }]
+  //       }, {
+  //         "featureType": "water",
+  //         "elementType": "geometry",
+  //         "stylers": [{
+  //           "color": "#a2daf2"
+  //         }]
+  //       }]
+  //     };
+  //     var mapElement = document.getElementById('map');
+  //     var map = new google.maps.Map(mapElement, mapOptions);
+  //     var marker = new google.maps.Marker({
+  //       position: new google.maps.LatLng(40.6700, -73.9400),
+  //       map: map,
+  //       // icon: 'assets/images/all-img/contact/map.png',
+  //       title: 'fugu'
+  //     });
+  //     var contentString = '<div id="content">' + '<div id="tpw">' + '<h3>fugu' + '</div>';
+  //     var infowindow = new google.maps.InfoWindow({
+  //       content: contentString,
+  //       maxWidth: 280
+  //     });
+  //     marker.setAnimation(google.maps.Animation.BOUNCE);
+  //     setTimeout(function () {
+  //       marker.setAnimation(null);
+  //     }, 750); //time it takes for one bounce   
 
-      google.maps.event.addListener(marker, 'click', function () {
-        infowindow.open(map, marker);
-      });
-    };
-    google.maps.event.addDomListener(window, 'load', init);
-  }
+  //     google.maps.event.addListener(marker, 'click', function () {
+  //       infowindow.open(map, marker);
+  //     });
+  //   };
+  //   google.maps.event.addDomListener(window, 'load', init);
+  // }
 
   //AXIMO wow js
   var wow = new WOW({
